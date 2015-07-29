edced59976841044c538df25f69029e13f408068
acidfunka <acidfunka1@gmail.com>
2015-07-29 13:31:35 +0200

    adding db values

--- t1.py
+++ t1.py
@@ -4,8 +4,8 @@ import sqlite3
 
 # Get the dataset
 # url could be param??
-#url = 'http://whereismypower.co.za/api/get_status.json'
-url = 'http://122.181.128.170:8080/cap-dataservice/rest/dataservice/1/CLM/1/CustomerProfiles/json/query'
+url = 'http://whereismypower.co.za/api/get_status.json'
+#url = 'http://122.181.128.170:8080/cap-dataservice/rest/dataservice/1/CLM/1/CustomerProfiles/json/query'
 response = urlopen(url)
 
 # Convert bytes to string type and string type to dict
@@ -14,29 +14,42 @@ json_obj = json.loads(string)
 
 ##print(json_obj) # prints the string with 'source_name' key
 json_clob = json.dumps(json_obj, sort_keys=True, indent=4)
-print(json_clob)
 
-conn = sqlite3.connect('powerDown.db')
-ide = 2
+stage = (json_obj['active_stage']) 
+stageName = (json_obj['active_stage_name'])
+
+
+print (stage,stageName)
+
+
+#print(json_clob)
+if stageName == 'No Load Shedding':
+   print ('Currently no load shedding')
+else:
+   print('We are currently on', stageName)
+#print(stageName)
+
+#conn = sqlite3.connect('powerDown.db')
+#ide = 2
 
 #conn.execute("INSERT INTO run (id,value) \
 #      VALUES (1, '5' )");
 
-conn.execute('delete from run')
+#conn.execute('delete from run')
 
-conn.execute('insert into run values (?,?)', (3,json_clob))
+#conn.execute('insert into run values (?,?)', (3,json_clob))
 
-conn.commit()
+#conn.commit()
 
-cursor = conn.execute("SELECT id, value from run")
-for row in cursor:
-   print ("id = ", row[0])
+#cursor = conn.execute("SELECT Stage from wschedule")
+#for row in cursor:
+   #print ("Stage = ", row[0])
    #print ("value = ", row[1], "\n")
    #print ("ADDRESS = ", row[2])
    #print ("SALARY = ", row[3], "\n")
 
 
-conn.close()
+#conn.close()
 
 
 ##data = json_obj
